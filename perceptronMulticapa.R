ed731cce7004f49b531d0cbef0bc7d90f46d68f4
Jesús Alejandro Yahuitl Rodríguez <jesus.yahuitl@alumno.buap.mx>
2021-04-23 05:09:40 -0500

    Uso de predict

--- perceptronMulticapa.R
+++ perceptronMulticapa.R
@@ -25,7 +25,7 @@ print(trainingdata)
 colnames(trainingdata) <- c("ojos_grandes","dientes_grandes","cara_peluda","arrugado_a","guapo_a","dulce_y_amable","grita","huye","busca_lenador","besa_en_la_mejilla","ofrece_alimento","coquetea","se_acerca")
 
 #Entrenamiento
-capas_ocultas = 7
+capas_ocultas = 12
 umbral = 0.01
 model <- neuralnet(grita+huye+busca_lenador+besa_en_la_mejilla+ofrece_alimento+coquetea+se_acerca~ojos_grandes+dientes_grandes+cara_peluda+arrugado_a+guapo_a+dulce_y_amable,trainingdata, hidden=capas_ocultas, threshold=umbral)
 
@@ -36,3 +36,18 @@ plot(model)
 final_output=cbind (ojos_grandes,dientes_grandes,cara_peluda,arrugado_a,guapo_a,dulce_y_amable,grita,huye,busca_lenador,besa_en_la_mejilla,ofrece_alimento,coquetea,se_acerca, as.data.frame(model$net.result) )
 colnames(final_output) = c("ojos_grandes","dientes_grandes","cara_peluda","arrugado_a","guapo_a","dulce_y_amable","grita","huye","busca_lenador","besa_en_la_mejilla","ofrece_alimento","coquetea","se_acerca","Ngrita","Nhuye","Nbusca_lenador","Nbesa_en_la_mejilla","Nofrece_alimento","Ncoquetea","Nse_acerca")
 print(final_output)
+
+#Tabla nueva
+#Características ligeramente diferentes
+ojos_grandes <-  as.data.frame(c(1,0,0))
+dientes_grandes <-  as.data.frame(c(1,0,0))
+cara_peluda <-  as.data.frame(c(1,0,1))
+arrugado_a <-  as.data.frame(c(1,1,0))
+guapo_a <- as.data.frame(c(0,1,1))
+dulce_y_amable <- as.data.frame(c(0,1,1))
+
+newdata <- cbind(ojos_grandes,dientes_grandes,cara_peluda,arrugado_a,guapo_a,dulce_y_amable)
+colnames(newdata) <- c("ojos_grandes","dientes_grandes","cara_peluda","arrugado_a","guapo_a","dulce_y_amable")
+
+
+predict(model, newdata, rep = 1, all.units = TRUE)
